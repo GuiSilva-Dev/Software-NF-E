c7880f551669837907a3edcbe4f8305cd54ecd82
Guilherme Silva <Guilhermelsp10@gmail.com>
2026-05-16 23:21:04 -0300

    Update Api.js

--- Front-End-NF-E/src/services/Api.js
+++ Front-End-NF-E/src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-  // Já incluímos o caminho base da API aqui
-  baseURL: (import.meta.env.VITE_API_URL || 'http://localhost:8080') + '/api/recibos'
+  // Tenta pegar a variável, se não achar, usa o localhost como segurança
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080'
 });
 
 export default Api;
